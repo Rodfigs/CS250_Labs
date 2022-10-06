5c794937503fcb7ea071f47873052cda10d1cf12
Rodfigs <69443128+Rodfigs@users.noreply.github.com>
2022-10-06 13:25:30 -0500

    Add files via upload

--- lab04/lab04b/src/lab04b/Lab04b.java
+++ lab04/lab04b/src/lab04b/Lab04b.java
@@ -4,6 +4,8 @@
  */
 package lab04b;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rodri
@@ -14,7 +16,34 @@ public class Lab04b {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Scanner Number = new Scanner(System.in);
+        int size;
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int key;
+        System.out.println("Please enter four numbers:");
+        num1 = Number.nextInt();
+        num2 = Number.nextInt();
+        num3 = Number.nextInt();
+        num4 = Number.nextInt();
+         int[] inputArray = new int[]{ num1,num2, num3, num4 }; 
+        
+        int arraySize = inputArray.length;
+       for (int i = 0; i < arraySize - 1; i++) {
+           int index = i;
+           for (int j = i + 1; j < arraySize; j++) {
+               if (inputArray[j] < inputArray[index]){
+               index = j;
+               }
+               }
+               int temp = inputArray[i];
+               inputArray[i] = inputArray[index];
+               inputArray[index] = temp;
+               }
+           }
+       }
+   
     
-}
+
